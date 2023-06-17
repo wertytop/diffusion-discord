56e6fed7d1ed57b9abab9668ec0ed359e8092793
wertytop <wertytop08@gmail.com>
2023-06-17 10:16:25 -0700

    it works now, added multiple images suppourt, etc

--- replicateapitest.py
+++ replicateapitest.py
@@ -1,6 +1,12 @@
 import replicate
+import os
 
-output = replicate.run("stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf:",
-     input={"prompt":"astronaut taking a selfie"}
+from dotenv import load_dotenv
+
+load_dotenv()
+replicate.Client(api_token=os.getenv("REPLICATE_API_TOKEN"))
+output = replicate.run(
+     "stability-ai/stable-diffusion:27b93a2413e7f36cd83da926f3656280b2931564ff050bf9575f1fdf9bcd7478",
+     input={"prompt": "a 19th century portrait of a wombat gentleman"}
 )
-print(output) 
+print(output)
